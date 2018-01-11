3cc434f80e77693c32b27e6e21fb9e73c58e3974
TylerIlunga <tylerilunga1@gmail.com>
2018-01-11 11:58:23 -0800

    homework00

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cmsi 186
+# CMSI 186
+
+## 2018 Computer Science course at Loyola Marymount University in Los Angeles, CA
---
+++ homework00/Greeter.java
@@ -0,0 +1,6 @@
+//Must match the name of the file
+public class Greeter {
+  public static void main( String[] args ) {
+    System.out.println("Hello, World!");
+  }
+}
