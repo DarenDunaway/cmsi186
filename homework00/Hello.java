bee42682fa787b68ea3c47e0533475e29745d3be
TylerIlunga <tylerilunga1@gmail.com>
2018-01-15 03:26:54 -0800

    file header

--- homework00/Hello.java
+++ homework00/Hello.java
@@ -1,3 +1,20 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Prints to the console "Hello, World!"; CMSI 186 Homework 00
+ *  Author        :  Tyler Ilunga
+ *  Date          :  2018-01-15
+ *  Description   :  See purpose above
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2018-01-15  Tyler Ilunga  File header reminder
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 public class Hello {
   public static void main( String[] args ) {
     System.out.println("Hello, World!");
